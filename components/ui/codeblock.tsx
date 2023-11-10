eecc7190973ac2ca9fcf13b8621a608f9f4f8e39
panoanx <panoan@outlook.com>
2023-11-10 18:49:52 +0800

    fix: code overflow on small screens; style: border radius for codeblock

--- components/ui/codeblock.tsx
+++ components/ui/codeblock.tsx
@@ -93,8 +93,8 @@ const CodeBlock: FC<Props> = memo(({ language, value }) => {
   }
 
   return (
-    <div className="relative w-full font-sans codeblock bg-zinc-950">
-      <div className="flex items-center justify-between w-full px-6 py-2 pr-4 bg-zinc-800 text-zinc-100">
+    <div className="relative w-full font-sans codeblock bg-zinc-950 rounded-md">
+      <div className="flex items-center justify-between w-full px-6 py-0 pr-4 bg-zinc-800 text-zinc-100 rounded-t-md">
         <span className="text-xs lowercase">{language}</span>
         <div className="flex items-center space-x-1">
           <Button
